7dbf7385ee347c3faa7264ac80895085d2fb8a3e
lancemadarang <lance.madarang@mail.utoronto.ca>
2022-12-14 18:42:32 -0500

    code smells MatchingAlgorithm

--- src/main/java/matching/MatchingAlgorithm.java
+++ src/main/java/matching/MatchingAlgorithm.java
@@ -25,36 +25,35 @@ public class MatchingAlgorithm {
         /*
          * Construct an arraylist of potential matches, giving them a score compatibility
          */
-
-        // set all user's score to 0
-        for (Account otherUser : otherUsers) {
-            otherUser.getProfile().setScore(0);
-        }
-
-        //get preferences
+        resetUserScores(otherUsers);
         HashMap<String, List<String>> preferences = user.getProfile().getStudyBuddyPreferences();
 
-        // assign scores to each user
         for (Account oUser : otherUsers) {
             for (String key : preferences.keySet()) {
                 assignScore(key, preferences, oUser.getProfile());
             }
         }
 
-        // sort users based on descending order of mathscore
-//        ArrayList<Account> matches = new ArrayList<Account>(); //potential matches
         Collections.sort(otherUsers);
+        LinkedList<Account> potentialMatches = createPotentialMatches(user, otherUsers);
 
+        return potentialMatches;
+    }
 
-        // create new LinkedList
+    private static LinkedList<Account> createPotentialMatches(Account user, ArrayList<Account> otherUsers) {
         LinkedList<Account> potentialMatches = new LinkedList<>();
         for (Account otherUser : otherUsers) {
             if (!user.getSeen().contains(otherUser)) {
                 potentialMatches.add(otherUser);
             }
         }
-
         return potentialMatches;
+    }
+
+    private static void resetUserScores(ArrayList<Account> otherUsers) {
+        for (Account otherUser : otherUsers) {
+            otherUser.getProfile().setScore(0);
+        }
     }
 
     public static void assignScore(String key, HashMap<String, List<String>> preferences, Profile oUser)  {
@@ -64,50 +63,60 @@ public class MatchingAlgorithm {
         * 3 keys: year, field, style
         * year preferences are sorted
          */
-        int score = oUser.getScore();
 
-        //compare years
         if (Objects.equals(key, "year")) {
-            if(!preferences.get("year").isEmpty()) {
-                ArrayList<String> years = (ArrayList<String>) preferences.get("year");
-                for (String year : years) {
-                    if (Objects.equals(year, oUser.getYear())) {
-                        score += 1;
-                    }
-                }
-            }
+            oUser.setScore(assignYearScore(preferences, oUser, oUser.getScore()));
         }
 
-        //compare field
         if (Objects.equals(key, "field")) {
-            if(!preferences.get("field").isEmpty()) {
-                ArrayList<String> field = (ArrayList<String>) preferences.get("field");
-                for (String s : field) {
-                    if (s.equals(oUser.getFieldOfStudy())) {
-                        score += 1;
-                    }
-                }
-            }
+            oUser.setScore(assignFieldScore(preferences, oUser, oUser.getScore()));
         }
 
-        //compare style
         if (Objects.equals(key, "style")) {
-            if(!preferences.get("style").isEmpty()) {
-                ArrayList<String> style = (ArrayList<String>) preferences.get("style");
-                for (String s : style) {
-                    if (oUser.getStudyStyles().contains(s)) {
-                        // style toString will return Arraylist of style
-                        score += 1;
-                    }
+            oUser.setScore(assignStyleScore(preferences, oUser, oUser.getScore()));
+        }
+
+    }
+
+    private static int assignStyleScore(HashMap<String, List<String>> preferences, Profile oUser, int score) {
+        if(!preferences.get("style").isEmpty()) {
+            ArrayList<String> style = (ArrayList<String>) preferences.get("style");
+            for (String s : style) {
+                if (oUser.getStudyStyles().contains(s)) {
+                    score += 1;
                 }
             }
         }
-        oUser.setScore(score);
+        return score;
+    }
+
+    private static int assignFieldScore(HashMap<String, List<String>> preferences, Profile oUser, int score) {
+        if(!preferences.get("field").isEmpty()) {
+            ArrayList<String> field = (ArrayList<String>) preferences.get("field");
+            for (String s : field) {
+                if (s.equals(oUser.getFieldOfStudy())) {
+                    score += 1;
+                }
+            }
+        }
+        return score;
+    }
+
+    private static int assignYearScore(HashMap<String, List<String>> preferences, Profile oUser, int score) {
+        if(!preferences.get("year").isEmpty()) {
+            ArrayList<String> years = (ArrayList<String>) preferences.get("year");
+            for (String year : years) {
+                if (Objects.equals(year, oUser.getYear())) {
+                    score += 1;
+                }
+            }
+        }
+        return score;
     }
 
     public static ArrayList<Account> getOthers() {
         /*
-        * method to get an arrayList of all other users excluding the current one
+         * method to get an arrayList of all other users excluding the current one
          */
         ArrayList<Account> otherUsers = new ArrayList<>();
         for (Account a : UserDatabase.getUserDatabase().getAccounts().values()) {
@@ -126,6 +135,3 @@ public class MatchingAlgorithm {
                 getOthers());
     }
 }
-
-
-
